75326848e40a34b765b784542d3bf0162a420d5e
Frank <122687187+TinkerFrank@users.noreply.github.com>
2023-11-29 22:14:22 +0000

    building gui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { getByTitle } from '@testing-library/react';
 import React from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    // Code to trigger image analysis
+    console.log('Image analysis triggered');
+  };
+
+  const handleImageGeneration = () => {
+    // Code to trigger image generation
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <div>
+      <h1>Title</h1>
+      <input type="text" value={imageUrl} onChange={handleImageUrlChange} placeholder="Enter image URL" />
+      <input type="text" value={prompt} onChange={handlePromptChange} placeholder="Enter prompt" />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
+
+
